71cd0ac777a838ede9bdbed6be524f89cd351e46
denniskioko <kiokodennis6990@gmail.com>
2024-04-14 19:50:01 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,59 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
+        return jsonify(messages), 200 
+    
+    elif request.method == 'POST':
+        data = request.json
+        username = data.get('username')
+        body = data.get('body')
 
-@app.route('/messages/<int:id>')
+        if not username or not body:
+            return jsonify({"error": "Username and body are required"}), 422
+
+        new_message = Message(username=username, body=body)
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        return jsonify(message_dict), 201
+    
+@app.route('/messages/<int:id>',methods= ['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    if request.method == 'PATCH':
+        data = request.json
+        new_body = data.get('body')
+
+        if not new_body:
+            return jsonify({"error": "New body is required"}), 422
+
+        message.body = new_body
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        return jsonify(message_dict), 200
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted"
+        }
+
+        return jsonify(response_body), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
